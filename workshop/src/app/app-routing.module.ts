4cf973dd46bdbafe6a34e54336e2493f8df4d428
Jean Gauthier-Damioli <jean.gauthier-damioli@epitech.eu>
2021-03-08 15:44:49 +0100

    [ADD] begin project

---
+++ workshop/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CryptoComponent } from './crypto/crypto.component';
+
+const routes: Routes = [
+  { path: '', component: CryptoComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
