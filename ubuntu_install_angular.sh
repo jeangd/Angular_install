29f95d39044825bf17b11f046c929eb3ac874714
Jean Gauthier-Damioli <jean.gauthier-damioli@epitech.eu>
2021-03-08 10:43:29 +0100

    [add] install for ubuntu

---
+++ ubuntu_install_angular.sh
@@ -0,0 +1,9 @@
+#!bash
+
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install npm
+npm install -g @angular/cli
+npm install -g @angular/cli@8
+sudo npm install -g @angular/cli@8
+npm install angular-lz-string
+ng --version
