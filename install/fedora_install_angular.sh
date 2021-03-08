772da970b24e50ad55689d7b0f35b518c730e8e0
Jean Gauthier-Damioli <jean.gauthier-damioli@epitech.eu>
2021-03-08 10:45:40 +0100

    [ADD] install for fedora

---
+++ install/fedora_install_angular.sh
@@ -0,0 +1,9 @@
+#!bash
+
+sudo dnf update && sudo dnf upgrade
+sudo dnf install npm
+npm install -g @angular/cli
+npm install -g @angular/cli@8
+sudo npm install -g @angular/cli@8
+npm install angular-lz-string
+ng --version
